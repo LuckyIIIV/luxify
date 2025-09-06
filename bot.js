7d3bc373a8556f4ffb9d02f5f660732e9c41077f
luckyiiiv <138688235+LuckyIIIV@users.noreply.github.com>
2025-09-06 19:01:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,9 +57,9 @@ function isBlacklisted(content) {
 }
 let ticketCounter = 0
 const ticketCategories = {
-  support: "1393207709117186178",
-  media: "1393207737747247184",
-  ban: "1393207740020555936"
+  Support: "1393207709117186178",
+  Apply: "1393207737747247184",
+  Ban: "1393207740020555936"
 }
 const allowedRoles = [
   "1412843820630016030",
@@ -320,7 +320,7 @@ client.on("interactionCreate", async interaction => {
       const logChannel = await channel.guild.channels.fetch(TICKET_LOG_CHANNEL)
       if (logChannel) {
         const logEmbed = new EmbedBuilder()
-          .setTitle(`🎫 Ticket #${channel.ticketNumber} Log`)
+          .setTitle(`🎫 #${channel.ticketType} Ticket Log`)
           .setDescription(`The ticket (${channel.ticketNumber}) was claimed by ${channel.claimer ? channel.claimer.tag : "No one"} and closed by ${interaction.user.tag} with reason:\n${reason}`)
           .setColor("Orange")
           .setTimestamp()
