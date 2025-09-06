f691f443db88e27b6708a64a6798ef6ec3e9fa3f
luckyiiiv <138688235+LuckyIIIV@users.noreply.github.com>
2025-09-06 19:20:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,10 +57,10 @@ function isBlacklisted(content) {
 }
 let ticketCounter = 0
 const ticketCategories = {
-  Support: "1393207709117186178",
-  Apply: "1393207737747247184",
-  Ban_Appeal: "1393207740020555936"
-};
+  support: "1393207709117186178",
+  apply: "1393207737747247184",
+  ban: "1393207740020555936"
+}
 const allowedRoles = [
   "1412843820630016030",
   "1393207670336651304",
@@ -189,13 +189,7 @@ client.on("interactionCreate", async interaction => {
     ticketCounter++
     const ticketNumber = String(ticketCounter).padStart(3, "0")
     const type = interaction.values[0]
-    
-    const categoryKey = Object.keys(ticketCategories).find(
-      key => key.toLowerCase() === type.toLowerCase()
-    );
-
     const categoryId = ticketCategories[categoryKey];
-
     if (!categoryId) {
       await interaction.editReply("❌ Category not found!");
       return;
